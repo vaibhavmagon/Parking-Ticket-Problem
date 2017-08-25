55def7492293f78c1a07acd4b1900ec8098a5cc3
vaibhavmagon <vaibhav210991@gmail.com>
2017-08-25 12:27:28 +0530

    minor fixes

--- parking_lot.sh
+++ parking_lot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-npm install
+npm --prefix $parent_path install
 npm --prefix $parent_path test
 if [ "$#" -ne 1 ]; 
 then
